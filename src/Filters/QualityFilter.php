d33b46586e69a6a8d5ba81a2868b5ff1b77a7c73
jeremytubbs <jeremytubbs@gmail.com>
2021-09-26 19:01:46 -0400

    undo

--- src/Filters/QualityFilter.php
+++ src/Filters/QualityFilter.php
@@ -40,10 +40,7 @@ class QualityFilter implements FilterInterface
         }
 
         if ($this->quality === 'bitonal') {
-            $image->limitColors(2);
-            $image->greyscale();
-            $image->contrast(100);
-            return $image->encode('bmp');
+            return $image->limitColors(2);
         }
     }
 }

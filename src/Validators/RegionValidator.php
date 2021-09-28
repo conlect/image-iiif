ccbe5dfda40149022c40b6724358304877a60851
jeremytubbs <jeremytubbs@gmail.com>
2021-09-28 06:10:03 -0400

    fix config

--- src/Validators/RegionValidator.php
+++ src/Validators/RegionValidator.php
@@ -9,6 +9,6 @@ class RegionValidator extends ValidatorAbstract implements ValidatorInterface
 {
     public function fails($region)
     {
-        return ! preg_match($this->config['iiif']['regex']['region'], $region);
+        return ! preg_match($this->config['regex']['region'], $region);
     }
 }

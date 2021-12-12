3d32c6a8ced1928010286ebb904aa953602a328d
jeremytubbs <jeremytubbs@users.noreply.github.com>
2021-12-12 12:21:43 +0000

    Fix styling

--- src/Validators/SizeValidator.php
+++ src/Validators/SizeValidator.php
@@ -3,10 +3,9 @@
 namespace Conlect\ImageIIIF\Validators;
 
 use Conlect\ImageIIIF\Validators\Contracts\ValidatorInterface;
-use Intervention\Image\Image;
 
 class SizeValidator extends ValidatorAbstract implements ValidatorInterface
-{   
+{
     protected $regex = [
         '^max$',
         '^\^max$',
@@ -22,15 +21,18 @@ class SizeValidator extends ValidatorAbstract implements ValidatorInterface
     {
         if (strpos($value, '^pct:') !== false) {
             $percent = preg_replace('/^\^pct:/', '', $value);
+
             return (int)$percent >= 1;
         }
 
         if (strpos($value, 'pct:') !== false) {
             $percent = preg_replace('/^pct:/', '', $value);
+
             return (int)$percent >= 1 && (int)$percent <= 100 ;
         }
 
         $all_regex = implode('|', $this->regex);
+
         return preg_match('/' . $all_regex . '/', $value) ? true : false;
     }
 }

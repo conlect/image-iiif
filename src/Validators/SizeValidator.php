ef81326263b9aa7ae448c15a3e01e30f374a8e4a
Jeremy Tubbs <jeremytubbs@gmail.com>
2023-03-12 20:49:49 -0400

    Scrutinizer bugs (#13)
    * fix bugs and complexity
    
    * Fix styling
    
    ---------
    
    Co-authored-by: jeremytubbs <jeremytubbs@users.noreply.github.com>

--- src/Validators/SizeValidator.php
+++ src/Validators/SizeValidator.php
@@ -8,9 +8,9 @@ use Conlect\ImageIIIF\Validators\Contracts\ValidatorInterface;
 
 class SizeValidator extends ValidatorAbstract implements ValidatorInterface
 {
+    protected $allow_upscaling;
+    protected $upscale;
     protected $regex = [
-        '^max$',
-        '^\^max$',
         ',[0-9]+',
         '\^,[0-9]+',
         '[0-9]+,',
@@ -18,42 +18,59 @@ class SizeValidator extends ValidatorAbstract implements ValidatorInterface
         '{!}?[0-9]+,[0-9]+',
     ];
 
+    public function __construct($config)
+    {
+        $this->allow_upscaling = $config['allow_upscaling'];
+    }
+
     public function valid($value)
     {
-        $startValue = $value;
+        if ($value === 'max') {
+            return true;
+        }
 
-        if (str_starts_with($value, '^') && $this->config['allow_upscaling'] === false) {
+        if ($value === '^max') {
+            throw new NotImplementedException("Maximum size is not implemented.");
+        }
+
+        if (! $this->allow_upscaling && $this->upscale) {
             throw new NotImplementedException("Upscaling is not allowed.");
         }
 
-        if (strpos($value, '^pct:') !== false) {
-            $value = preg_replace('/^\^pct:/', '', $value);
+        $this->upscale = str_starts_with($value, '^');
+        $isPercent = str_contains($value, 'pct:');
+        $percent_value = (int)$this->getPercentValue($value);
 
-            if ((int)$value == 0) {
-                throw new BadRequestException("Size $startValue is invalid.");
+        if ($isPercent) {
+            if ($percent_value == 0 || $percent_value < 1) {
+                throw new BadRequestException("Size $value is invalid.");
             }
-            if ((int)$value >= 1) {
+            if ($this->upscale) {
                 return true;
             }
-        }
-
-        if (strpos($value, 'pct:') !== false) {
-            $value = preg_replace('/pct:/', '', $value);
-
-            if ((int)$value == 0 || (int)$value > 100) {
-                throw new BadRequestException("Size $startValue is invalid.");
+            if ($percent_value > 100) {
+                throw new BadRequestException("Size $value is invalid.");
             }
-            if ((int)$value >= 1 && (int)$value <= 100) {
+
+            if ($percent_value <= 100) {
                 return true;
             }
         }
 
         $all_regex = implode('|', $this->regex);
-
         if (preg_match('/' . $all_regex . '/', $value)) {
             return true;
         }
 
-        throw new BadRequestException("Size $startValue is invalid.");
+        throw new BadRequestException("Size $value is invalid.");
+    }
+
+    protected function getPercentValue($value)
+    {
+        if ($this->upscale) {
+            return preg_replace('/^\^pct:/', '', $value);
+        }
+
+        return preg_replace('/pct:/', '', $value);
     }
 }

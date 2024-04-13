627800d140fe4b9fd427bdbb0c567cf7db21b03a
Jeremy Tubbs <jeremytubbs@gmail.com>
2024-04-13 19:50:17 -0400

    quality test

---
+++ tests/QualityTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Conlect\ImageIIIF\Tests;
+
+use Conlect\ImageIIIF\Filters\QualityFilter;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
+use PHPUnit\Framework\TestCase;
+
+class QualityTest extends TestCase
+{
+    public function test_it_returns_default()
+    {
+        $manager = new ImageManager(new Driver());
+        $image = $manager->read('tests/data/image.png');
+        $filter = new QualityFilter('default');
+        $result = $filter->applyFilter($image);
+
+        $this->assertEquals($image, $result);
+    }
+
+    public function test_it_returns_color()
+    {
+        $manager = new ImageManager(new Driver());
+        $image = $manager->read('tests/data/image.png');
+        $filter = new QualityFilter('color');
+        $result = $filter->applyFilter($image);
+
+        $this->assertEquals($image, $result);
+    }
+
+    public function test_it_returns_gray()
+    {
+        $manager = new ImageManager(new Driver());
+        $image = $manager->read('tests/data/image.png');
+        $filter = new QualityFilter('gray');
+        $result = $filter->applyFilter($image);
+
+        // test that the image is grayscale
+        $color = $result->pickColor(random_int(0, $result->width()), random_int(0, $result->height()))->toHex();
+
+        // Break the color into its components
+        $r = hexdec(substr($color, 0, 2));
+        $g = hexdec(substr($color, 2, 2));
+        $b = hexdec(substr($color, 4, 2));
+        $this->assertEquals($r, $g);
+        $this->assertEquals($g, $b);
+    }
+}

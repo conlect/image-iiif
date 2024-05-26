aac4591d5f29e2a2709b7906479c8001a020eba6
jeremytubbs <jeremytubbs@users.noreply.github.com>
2024-05-26 18:21:25 +0000

    Fix styling

--- src/Validators/FormatValidator.php
+++ src/Validators/FormatValidator.php
@@ -8,6 +8,7 @@ use Conlect\ImageIIIF\Validators\Contracts\ValidatorInterface;
 class FormatValidator extends ValidatorAbstract implements ValidatorInterface
 {
     use \Conlect\ImageIIIF\Traits\SupportedFormats;
+
     public function valid($format)
     {
         $formats = $this->getSupportedFormats($this->config['driver']);
